6a75028b528b27c515895b85a79ce0e97835fb3e
Ecem Ozturk <ecemozturk704@gmail.com>
2023-10-05 21:50:26 +0300

    Initial Commit

---
+++ ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by Ecem Öztürk on 5.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
