b655d273c740b7dae40acdc4745a3d80dae3fcea
Ecem Ozturk <ecemozturk704@gmail.com>
2023-10-07 21:50:09 +0300

    added chat user interface

--- ChatBot/Chat/ChatViewModel.swift
+++ ChatBot/Chat/ChatViewModel.swift
@@ -6,11 +6,34 @@
 //
 
 import Foundation
+import OpenAI
 
-class ChatViewModel {
+class ChatViewModel: ObservableObject {
+    @Published var chat: AppChat?
+    @Published var messages: [AppMessage] = []
+    @Published var messageText: String = ""
+    @Published var selectedModel: ChatModel = .gpt3_5_turbo
     let chatID: String
     
     init (chatID: String) {
         self.chatID = chatID
     }
+    
+    func fetchData() {
+        self.messages = [AppMessage(id: "1", text: "Hello hi", role: .user, createdAt: Date()),
+        AppMessage(id: "2", text: "Hellooooo", role: .assistant, createdAt: Date())]
+    }
+    
+    func sendMessage() {
+        var newMessage = AppMessage(id: UUID().uuidString, text: messageText, role: .user, createdAt: Date())
+        messages.append(newMessage)
+        messageText = ""
+    }
+}
+
+struct AppMessage: Identifiable, Codable, Hashable {
+    let id: String?
+    var text: String
+    let role: Chat.Role
+    let createdAt: Date
 }
